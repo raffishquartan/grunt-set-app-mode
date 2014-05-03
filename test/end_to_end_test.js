cbcdeed0bc0f52c2f8943a21cac9cabe7e3e6523
Christo Fogelberg <cgf@syntilect.com>
2014-05-03 11:36:56 +0100

    Initial failing end to end tests (skipped, but missing class definitions)

--- test/end_to_end_test.js
+++ test/end_to_end_test.js
@@ -1,35 +1,82 @@
-describe("grunt-set-app-mode", function() {
-  describe("build with valid mode creates output", function() {
-    grunt.initConfig({
-      set_app_mode: {
-        files: [
-          {
-            src: ["test/src/config.{{MODE}}.js"]
-            dest: ["tmp"]
-          }
-        ]
-      }
-    });
+"use strict";
 
-    // NOW DO BUILD IN HERE - MODE = DEV
+describe("end-to-end tests", function() {
+  var grunt = require("grunt");
+  var should = require("should");;
 
-    // ASSERT FILES ARE EQUAL AT END
+  function content_of(file_a) {
+    return false;
+  };
+
+  describe.skip("build with valid mode creates output", function() {
+    var configValid = {
+      files: [
+        {
+          src: ["test/src/config.{{MODE}}.js"],
+          dest: ["tmp"]
+        }
+      ]
+    };
+
+    var set_task = new GruntSetAppMode(config);
+    grunt.task.run(set_task);
+    var file_a = content_of("test/src/config.dev.js");
+    var file_b = content_of("tmp/config.js");
+    file_a.should.eql.file_b;
   });
 
-  describe("build with invalid mode raises error", function() {
-    grunt.initConfig({
-      set_app_mode: {
-        files: [
-          {
-            src: ["test/src/config.{{MODE}}.js"]
-            dest: ["tmp"]
-          }
-        ]
-      }
-    });
+  describe.skip("build with invalid mode fails task", function() {
+    var configValid = {
+      files: [
+        {
+          src: ["test/src/config.{{MODE}}.js"],
+          dest: ["tmp"]
+        }
+      ]
+    };
 
-    // NOW DO BUILD IN HERE - MODE = DEVELOPMENT
+    var set_task = GruntSetAppMode(config);
+    grunt.task.run(set_task);
+    // ASSERT task failed - how does grunt notify of task failure?
+  });
 
-    // ASSERT throws error at end
+  describe.skip("missing dest in configuration files array object element fails task", function() {
+    var config_no_src = {
+      files: [
+        {
+          src: ["test/src/config.{{MODE}}.js"]
+        }
+      ]
+    };
+
+    // this.requiresConfig inside a task...
+  });
+
+  describe.skip("missing src in configuration files array object element fails task", function() {
+    var config_no_src = {
+      files: [
+        {
+          dest: ["tmp"]
+        }
+      ]
+    };
+
+    // test this.requiresConfig inside a task
+  });
+
+  describe.skip("specifying no objects in configuration files array raises warning", function() {
+    var config_no_entries = {
+      files: [
+      ]
+    };
+
+    // this inside a task
+  });
+
+  describe.skip("specifying no configuration files array raises warning", function() {
+    var config_no_entries = {
+    };
+
+    // this inside a task
   });
 });

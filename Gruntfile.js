10c6da8a9880bbc122c53916a72c7544fa5ef8b5
Christo Fogelberg <cgf@syntilect.com>
2014-05-07 22:00:54 +0100

    Gruntfile.js: ' --> "

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+"use strict";
 
 module.exports = function(grunt) {
 
@@ -14,18 +14,18 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
+        "Gruntfile.js",
+        "tasks/*.js",
+        "<%= nodeunit.tests %>",
       ],
       options: {
-        jshintrc: '.jshintrc',
+        jshintrc: ".jshintrc",
       },
     },
 
     // Before generating any new files, remove any previously-created files.
     clean: {
-      tests: ['tmp'],
+      tests: ["tmp"],
     },
 
     // Configuration to be run (and then tested).
@@ -34,16 +34,16 @@ module.exports = function(grunt) {
         options: {
         },
         files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
+          "tmp/default_options": ["test/fixtures/testing", "test/fixtures/123"],
         },
       },
       custom_options: {
         options: {
-          separator: ': ',
-          punctuation: ' !!!',
+          separator: ": ",
+          punctuation: " !!!",
         },
         files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
+          "tmp/custom_options": ["test/fixtures/testing", "test/fixtures/123"],
         },
       },
     },
@@ -51,29 +51,29 @@ module.exports = function(grunt) {
     mochaTest: {
       test: {
         options: {
-          reporter: 'spec',
+          reporter: "spec",
           clearRequireCache: true
         },
-        src: ['test/**/*.js']
+        src: ["test/**/*.js"]
       },
     }
   });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+  // Actually load this plugin"s task(s).
+  grunt.loadTasks("tasks");
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-nodeunit");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-mocha-test");
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'set_app_mode', 'nodeunit']);
+  // plugin"s task(s), then test the result.
+  grunt.registerTask("test", ["clean", "set_app_mode", "nodeunit"]);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask("default", ["jshint", "test"]);
 
 };

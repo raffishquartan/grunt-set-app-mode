ff11dceb0306909e0a8d2a78cba6e931bde09d6f
Christo Fogelberg <cgf@syntilect.com>
2014-05-08 19:39:26 +0100

    SetAppModeConfig - initial working version, along with fixed unit tests; added underscore to devDependencies

--- test/unit/set_app_mode_config_test.js
+++ test/unit/set_app_mode_config_test.js
@@ -1,24 +1,31 @@
 "use strict";
 
-describe("set app mode config unit tests", function() {
+describe("SetAppModeConfig unit tests", function() {
+  var _ = require("underscore");
   var should = require("should");
-  var SetAppModeConfig = require("../lib/SetAppModeConfig");
+  var SetAppModeConfig = require("../../lib/set_app_mode_config");
+
+
 
   var VALID_EXPECTED_MODES = [ "bronze", "silver", "gold" ];
   var VALID_MODE = "gold";
-  var UNKNOWN_MODE = "platinum"
+  var UNKNOWN_MODE = "platinum";
 
   var VALID_CONFIGURATION = {
     expected_modes: VALID_EXPECTED_MODES,
     mode: VALID_MODE,
   };
 
-  var UNKNOWN_MODE_CONFIGURATION = {
-    expected_modes: VALID_EXPECTED_MODES,
-    mode: UNKNOWN_MODE,
+  var NO_MODE_CONFIGURATION = {
+    expected_modes: VALID_EXPECTED_MODES
   };
 
   var NO_EXPECTED_MODES_CONFIGURATION = {
+    mode: VALID_MODE
+  };
+
+  var EXPECTED_MODES_IS_NOT_AN_ARRAY = {
+    expected_modes: VALID_MODE,
     mode: VALID_MODE
   };
 
@@ -27,35 +34,52 @@ describe("set app mode config unit tests", function() {
     mode: VALID_MODE
   };
 
-  var NO_MODE_CONFIGURATION = {
-    expected_modes: VALID_EXPECTED_MODES
+  var UNKNOWN_MODE_CONFIGURATION = {
+    expected_modes: VALID_EXPECTED_MODES,
+    mode: UNKNOWN_MODE,
   };
 
-  it.skip("stores the mode 'gold' when using VALID_CONFIGURATION", function() {
+
+
+  it("stores the mode 'gold' when using VALID_CONFIGURATION", function() {
     var config = new SetAppModeConfig(VALID_CONFIGURATION);
-    SetAppModeConfig.get_mode().should.equal("smart");
+    config.get_mode().should.equal(VALID_MODE);
   });
 
-  it.skip("stores the correct expected_modes when using VALID_CONFIGURATION", function() {
+  it("stores the correct expected_modes when using VALID_CONFIGURATION", function() {
     var config = new SetAppModeConfig(VALID_CONFIGURATION);
-    SetAppModeConfig.get_expected_modes().should.containEql("dev");
-    SetAppModeConfig.get_expected_modes().should.containEql("staging");
-    SetAppModeConfig.get_expected_modes().should.containEql("prod");
+    _.each(VALID_EXPECTED_MODES, function(a_valid_mode) {
+      config.get_expected_modes().should.containEql(a_valid_mode);
+    });
   });
 
-  it.skip("throws an Error when the specified mode is not in the list of expected modes", function() {
-    var config = new SetAppModeConfig(UNKNOWN_MODE_CONFIGURATION).should.throw();
+  it("throws an Error when mode is not set", function() {
+    (function() {
+      var config = new SetAppModeConfig(NO_MODE_CONFIGURATION)
+    }).should.throw();
   });
 
-  it.skip("throws an Error when the list of expected_modes is empty", function() {
-    var config = new SetAppModeConfig(EMPTY_EXPECTED_MODES_CONFIGURATION).should.throw();
+  it("throws an Error when list of expected_modes is not present", function() {
+    (function() {
+      var config = new SetAppModeConfig(NO_EXPECTED_MODES_CONFIGURATION).should.throw();
+    }).should.throw();
   });
 
-  it.skip("throws an Error when the list of expected_modes is not present", function() {
-    var config = new SetAppModeConfig(NO_EXPECTED_MODES_CONFIGURATION).should.throw();
+  it("throws an Error when expected_modes is not an array", function() {
+    (function() {
+      var config = new SetAppModeConfig(EXPECTED_MODES_IS_NOT_AN_ARRAY).should.throw();
+    }).should.throw();
   });
 
-  it.skip("throws an Error when the mode is not set", function() {
-    var config = new SetAppModeConfig(NO_MODE_CONFIGURATION).should.throw();
+  it("throws an Error when list of expected_modes is empty", function() {
+    (function() {
+      var config = new SetAppModeConfig(EMPTY_EXPECTED_MODES_CONFIGURATION).should.throw();
+    }).should.throw();
+  });
+
+  it("throws an Error when specified mode is not in the list of expected modes", function() {
+    (function() {
+      var config = new SetAppModeConfig(UNKNOWN_MODE_CONFIGURATION).should.throw();
+    }).should.throw();
   });
 });

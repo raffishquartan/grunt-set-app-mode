891657441d07a2c480684e491fb3e592851e6a61
Christo Fogelberg <cgf@syntilect.com>
2014-05-11 13:44:28 +0100

    ModeGroupConfigCollection tests and implementation

--- test/unit/mode_group_config_collection_test.js
+++ test/unit/mode_group_config_collection_test.js
@@ -8,19 +8,52 @@
 
 "use strict";
 
-describe("ModeGroupGonfigCollection unit tests", function() {
+describe("ModeGroupGonfigCollection", function() {
   var should = require("should");
-  var SetAppModeConfig = require("../../lib/set_app_mode_config");
+  var ModeGroupConfig = require("../../lib/mode_group_config");
+  var ModeGroupConfigCollection = require("../../lib/mode_group_config_collection");
+
+
+  var SRC_DIR = "test/src";
+  var DEST_DIR = "test/tmp";
+  var SRC_METAL_PREFIX_A = "mode";
+  var SRC_METAL_PREFIX_B = "other";
+  var EXPECTED_MODES_METAL = [ "bronze", "silver", "gold" ];
+
+  var MODE_GROUP_CONFIG_A = {
+    orig: {
+      src: SRC_DIR + "/mode.{{MODE}}.js",
+      dest: DEST_DIR
+    }
+  }
+  var MODE_GROUP_CONFIG_B  = {
+    orig: {
+      src: SRC_DIR + "/other.{{MODE}}.js",
+      dest: DEST_DIR
+    }
+  }
+  var VALID_FILES_ARRAY_METAL = [ MODE_GROUP_CONFIG_A, MODE_GROUP_CONFIG_B ];
 
 
 
-  // variables
+  it("has the correct length", function() {
+    var mgcc = new ModeGroupConfigCollection(VALID_FILES_ARRAY_METAL, EXPECTED_MODES_METAL);
+    mgcc.length().should.equal(2);
+  });
 
+  it("can be iterated over without errors", function() {
+    var mgcc = new ModeGroupConfigCollection(VALID_FILES_ARRAY_METAL, EXPECTED_MODES_METAL);
+    for(var i = 0; i < mgcc.length(); ++i) {
+      mgcc.get(i);
+    }
+  });
 
+  it("builds mode group config objects in the constructor parameter order", function() {
+    var mgcc = new ModeGroupConfigCollection(VALID_FILES_ARRAY_METAL, EXPECTED_MODES_METAL);
+    var mode_group_config_a = new ModeGroupConfig(MODE_GROUP_CONFIG_A.orig, EXPECTED_MODES_METAL);
+    var mode_group_config_b = new ModeGroupConfig(MODE_GROUP_CONFIG_B.orig, EXPECTED_MODES_METAL);
 
-  // functions
-
-
-
-  // tests
+    mgcc.get(0).should.eql(mode_group_config_a);
+    mgcc.get(1).should.eql(mode_group_config_b);
+  });
 });

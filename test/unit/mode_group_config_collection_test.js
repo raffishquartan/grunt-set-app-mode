37147ed7a5f520eba807e8aeaaa9a0f46c8081b2
Christo Fogelberg <cgf@syntilect.com>
2014-05-11 12:29:05 +0100

    ModeGroupConfigCollection tests and impl stubbed out

---
+++ test/unit/mode_group_config_collection_test.js
@@ -0,0 +1,26 @@
+/*
+ * grunt-set-app-mode
+ * https://github.com/cfogelberg/grunt-set-app-mode
+ *
+ * Copyright (c) 2014 Christo Fogelberg
+ * Licensed under the MIT license.
+ */
+
+"use strict";
+
+describe("ModeGroupGonfigCollection unit tests", function() {
+  var should = require("should");
+  var SetAppModeConfig = require("../../lib/set_app_mode_config");
+
+
+
+  // variables
+
+
+
+  // functions
+
+
+
+  // tests
+});

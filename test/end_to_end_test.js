b18d7d9346eff1456a03c12b693df345369f9b07
Christo Fogelberg <cgf@syntilect.com>
2014-05-11 18:27:28 +0100

    end_to_end_test.js - removed test which runs task through grunt

--- test/end_to_end_test.js
+++ test/end_to_end_test.js
@@ -2,37 +2,8 @@
 
 describe("end-to-end tests (grunt)", function() {
   var should = require("should");
-  var grunt; // require before each test to ensure it is fresh
-  var Validators = require("../test/lib/validators");
+  var grunt = require("grunt");
   var GruntSetAppMode = require("../lib/grunt_set_app_mode");
-
-  var TEST_CONFIGURATION = {
-    set_app_mode: {
-      options: {
-        mode: "dev" // hard code target mode, expected_modes defaults to dev, staging, prod
-      },
-      files: [
-        {
-          src: ["src/config.{{MODE}}.js"],
-          dest: ["tmp"]
-        }
-      ]
-    }
-  };
-
-  function load_file_contents(filepath) {
-    return grunt.file.exists(filepath) ? grunt.file.read(filepath) : undefined;
-  };
-
-
-
-  beforeEach(function() {
-    grunt = require("grunt");
-    if(grunt.file.exists("tmp")) {
-      grunt.file.delete("tmp");
-    }
-  });
-
 
 
   it("registers itself with grunt", function() {
@@ -40,14 +11,5 @@ describe("end-to-end tests (grunt)", function() {
     GruntSetAppMode.registerWithGrunt(grunt);
     should.exist(grunt.task._tasks[GruntSetAppMode.taskName]);
     grunt.task._tasks[GruntSetAppMode.taskName].info.should.equal(GruntSetAppMode.taskDescription);
-  });
-
-  // TODO: Get the mochaTest task to wait on set_app_mode to finish (currently queuing runs it after by default)
-  it.skip("copies target-specific mode file and removes mode-specific files", function() {
-    GruntSetAppMode.registerWithGrunt(grunt);
-    grunt.initConfig(TEST_CONFIGURATION);
-    grunt.task.run("set_app_mode");
-
-    Validators.check_final_result(["dev", "staging", "prod"], "config", "dev", "tmp", "src");
   });
 });

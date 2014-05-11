a0f9d28062147cf58d03d7d1b9de88246689d2f5
Christo Fogelberg <cgf@syntilect.com>
2014-05-11 12:28:09 +0100

    Gruntfile.js tidied and simplified

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,20 +32,9 @@ module.exports = function(grunt) {
     }
   });
 
-  // Actually load this plugin"s task(s).
   grunt.loadTasks("tasks");
-
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-contrib-nodeunit");
-  grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-mocha-test");
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin"s task(s), then test the result.
-  grunt.registerTask("test", ["clean", "set_app_mode", "nodeunit"]);
-
-  // By default, lint and run all tests.
-  grunt.registerTask("default", ["jshint", "test"]);
-
+  grunt.registerTask("test", ["mochaTest"]);
+  grunt.registerTask("default", ["test"]);
 };

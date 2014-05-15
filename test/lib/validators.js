6ff84f2eba14ec0761d7b5cf65316e02b265d814
Christo Fogelberg <cgf@syntilect.com>
2014-05-15 11:24:31 +0100

    Implemented trailing .{{MODE}}, refactored validator to allow tests for mode's not wrapped in periods

--- test/lib/validators.js
+++ test/lib/validators.js
@@ -1,4 +1,5 @@
 var grunt = require("grunt");
+var path = require("path");
 
 function load_file_contents(filepath) {
   if(grunt.file.exists(filepath)) {
@@ -10,20 +11,20 @@ function load_file_contents(filepath) {
 };
 
 var Validators = {
-  check_final_result: function(expected_modes, prefix, mode, dest_dir, src_dir) {
-    if(expected_modes === undefined || prefix === undefined || mode === undefined || dest_dir === undefined ||
-      src_dir === undefined) {
+  check_final_result: function(expected_modes, src_mode_glob, mode, dest_dir, src_dir) {
+    if(expected_modes === undefined || src_mode_glob === undefined || mode === undefined ||
+      dest_dir === undefined || src_dir === undefined) {
       throw new Error("Error: One of Valiators.check_final_result parameters is undefined");
     }
 
     expected_modes.forEach(function(a_mode) {
-      var invalid_filepath = dest_dir + "/" + prefix + "." + a_mode + ".js"
+      var invalid_filepath = path.join(dest_dir, src_mode_glob.replace("{{MODE}}", a_mode));
       grunt.file.exists(invalid_filepath).should.equal(false, invalid_filepath + " should not exist");
     });
-    var valid_filepath = dest_dir + "/" + prefix + ".js";
+    var valid_filepath = path.join(dest_dir, src_mode_glob.replace(".{{MODE}}", ""));
     grunt.file.exists(valid_filepath).should.equal(true, valid_filepath + " should exist");
 
-    var valid_src_filepath = src_dir + "/" + prefix + "." + mode + ".js";
+    var valid_src_filepath = path.join(src_dir, src_mode_glob.replace("{{MODE}}", mode));
     var content_of_src = load_file_contents(valid_src_filepath);
     var content_of_dest = load_file_contents(valid_filepath);
     content_of_src.should.equal(content_of_dest, valid_filepath + " contents should equal " +

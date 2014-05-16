981c91726bde003a79b241d78c2bfc59004d3a09
Christo Fogelberg <cgf@syntilect.com>
2014-05-16 22:24:47 +0100

    blanket.js wrapper added, .gitignore no longer ignores everything in test

---
+++ test/blanket.js
@@ -0,0 +1,5 @@
+var path = require('path');
+require('blanket')({
+  // Only files that match the pattern will be instrumented
+  pattern: [ path.join(__dirname, "..", "lib"), path.join(__dirname, "..", "tasks") ]
+});
